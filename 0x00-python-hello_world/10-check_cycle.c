f28ab5636590772edf01b4e4be5ccc3c5a3ec614
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-10-31 03:48:46 +0200

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
 	listint_t *curr, *suspect;
 
 	for (i = 0, curr = list; curr->next; curr = curr->next, i++)
-		for (l = 0, suspect = list; l <= i; suspect = suspect->next, l++)
+		for (l = 0, suspect = list; l < i; suspect = suspect->next, l++)
 			if (suspect == curr)
 				return (1);
 	return (0);
