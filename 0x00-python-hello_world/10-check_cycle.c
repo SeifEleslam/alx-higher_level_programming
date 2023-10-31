b36dc8b20985b8ff3b77bd71aa510c1335b525f6
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-10-31 03:43:53 +0200

    Create 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+/**
+ * check_cycle - prints all elements of a listint_t list
+ * @listint_t: pointer to head of list
+ * Return: int 
+ */
+
+int check_cycle(listint_t *list)
+{
+	unsigned int i, l;
+	listint_t *curr, *suspect;
+
+	for (i = 0, curr = list; curr->next; curr = curr->next, i++)
+		for (l = 0, suspect = list; l <= i; suspect = suspect->next, l++)
+			if (suspect == curr)
+				return (1);
+	return (0);
+}
