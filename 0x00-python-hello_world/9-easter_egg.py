0a75ac1782617bc7d1516fe373443aba7eaf563f
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-10-31 02:55:37 +0200

    Update 9-easter_egg.py

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import this
-
-if this.__doc__ is not None:
-  arr = this.__doc__.split('\n\n')
+arr = this.__doc__
+if arr:
+  arr = arr.split('\n\n')
   print(arr[2])
   print(arr[1])
