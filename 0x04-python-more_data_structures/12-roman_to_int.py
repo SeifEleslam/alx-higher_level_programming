ea96c9cf4bb1674993e44405af2e95efb87b9a19
SeifEleslam <seifgouda98@gmail.com>
2023-11-09 02:18:09 +0200

    test'

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dictionary = {'I': 1, 'V': 5, 'X': 10,
+                        'L': 50, 'C': 100, 'D': 500, 'M': 1000, }
+    sum, prv_val, curr_set = 0, 0, 0
+    for letter in roman_string:
+        value = roman_dictionary[letter]
+        if value > prv_val:
+            curr_set = value - curr_set
+        elif prv_val > value:
+            sum += curr_set
+            curr_set = value
+        else:
+            curr_set += value
+        prv_val = value
+    sum += curr_set
+    return sum
+
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
