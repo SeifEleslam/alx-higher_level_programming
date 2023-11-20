c0b433a062f1b38a30f518abf4ade5eb1b88abf7
SeifEleslam <seifgouda98@gmail.com>
2023-11-20 15:52:50 +0200

    test

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printedElements = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printedElements += 1
+        except:
+            break
+    print()
+    return printedElements
