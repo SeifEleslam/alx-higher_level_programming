6be114545e5f128dff3b818cb30273672a510c8b
SeifEleslam <seifgouda98@gmail.com>
2023-11-20 16:05:49 +0200

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(int(my_list[i])), end="")
             printedElements += 1
-        except (KeyError, ValueError):
+        except (KeyError, ValueError, TypeError):
             break
 
     print()
