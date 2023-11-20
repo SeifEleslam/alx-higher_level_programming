13c73efd1e890470bbd0bf6e4b61f187ab83838b
SeifEleslam <seifgouda98@gmail.com>
2023-11-20 20:24:21 +0200

    task 4

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             printedElements += 1
         except (ValueError, TypeError):
             continue
-        except Exception:
-            break
 
     print()
     return printedElements
