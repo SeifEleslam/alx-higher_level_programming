c2870d531147e368e36acf20fadd971052213c91
SeifEleslam <seifgouda98@gmail.com>
2023-11-21 16:48:46 +0200

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,19 +54,3 @@ class Square:
         print("\n" * self.position[1], end="")
         for i in range(self.size):
             print("{}{}".format(" " * (self.position[0]), "#" * (self.size)))
-
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
