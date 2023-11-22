402b7140f09949a7254fc6e4ac5ecf277c181e9e
SeifEleslam <seifgouda98@gmail.com>
2023-11-22 02:59:02 +0200

    task 6

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -64,18 +64,3 @@ class SinglyLinkedList:
             output += str(tmp.data) + ("\n" if tmp.next_node else "")
             tmp = tmp.next_node
         return output
-
-
-# sll = SinglyLinkedList()
-# sll.sorted_insert(2)
-# sll.sorted_insert(5)
-# sll.sorted_insert(3)
-# sll.sorted_insert(10)
-# sll.sorted_insert(1)
-# sll.sorted_insert(-4)
-# sll.sorted_insert(-3)
-# sll.sorted_insert(4)
-# sll.sorted_insert(5)
-# sll.sorted_insert(12)
-# sll.sorted_insert(3)
-# print(sll)
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Module of first task that contains class square"""
+
+
+class Square:
+    """Represents a square that has a private instance attribute size."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Intialize size of square with raising the proper error"""
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Calculate Square Area"""
+        return self.size ** 2
+
+    @property
+    def size(self):
+        """Get the value of Size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Get the value of Size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the value of Size"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Get the value of Size"""
+        if len(value) != 2 or not self.is_tuple_valid(value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def is_tuple_valid(self, tup):
+        for ele in tup:
+            if (not isinstance(ele, int)) or ele < 0:
+                return False
+        return True
+
+    def my_print(self):
+        """Print the Square with char #"""
+        if self.size == 0:
+            print()
+            return
+        print("\n" * self.position[1], end="")
+        for i in range(self.size):
+            print("{}{}".format(" " * (self.position[0]), "#" * (self.size)))
+
+    def __repr__(self):
+        """String represintaion for square"""
+        output = ""
+        output += "\n" * self.position[1]
+        for i in range(self.size):
+            output += "{}{}\n".format(" " *
+                                      (self.position[0]), "#" * (self.size))
+        return "" if self.size == 0 else output[0:-1]
