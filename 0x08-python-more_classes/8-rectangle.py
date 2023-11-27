a3a2873d5b9ebc7ea56f8347f17be60071c4f051
SeifEleslam <seifgouda98@gmail.com>
2023-11-27 21:34:19 +0200

    task3

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -8,6 +8,14 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+    def bigger_or_equal(rect_1, rect_2):
+        """Comparing two Recs and returning the bigger"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_2 if rect_2.area() > rect_1.area() else rect_1
+
     def __init__(self, width=0, height=0):
         """Intialize size of square with raising the proper error"""
         self.height = height
