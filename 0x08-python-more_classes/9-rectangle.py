623a43e7cfc1b4da1d738741a8d2d55dd76f8caa
SeifEleslam <seifgouda98@gmail.com>
2023-11-27 22:20:53 +0200

    task9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Creates new rectangle with given size or default 0"""
-        return cls(size, size)
+        return Rectangle(size, size)
 
     def bigger_or_equal(rect_1, rect_2):
         """Comparing two Recs and returning the bigger"""
