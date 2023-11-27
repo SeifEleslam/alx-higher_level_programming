af81034cead68ded33da3b31308e448a76d68d3b
SeifEleslam <seifgouda98@gmail.com>
2023-11-27 20:52:00 +0200

    task3

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -63,4 +63,4 @@ class Rectangle:
 
     def __repr__(self):
         """New Instance Code"""
-        return "Rectangle({},{})".format(self.width, self.height)
+        return "Rectangle({}, {})".format(self.width, self.height)
