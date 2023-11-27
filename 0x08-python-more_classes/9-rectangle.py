e47edf5b306b02e7b4fae446df5f21fc7631b661
SeifEleslam <seifgouda98@gmail.com>
2023-11-27 22:43:23 +0200

    task9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -8,6 +8,15 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Comparing two Recs and returning the bigger"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_2 if rect_2.area() > rect_1.area() else rect_1
+
     def __init__(self, width=0, height=0):
         """Intialize size of square with raising the proper error"""
         self.height = height
@@ -53,17 +62,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """Creates new rectangle with given size or default 0"""
         return cls(size, size)
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Comparing two Recs and returning the bigger"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        return rect_2 if rect_2.area() > rect_1.area() else rect_1
 
     def __str__(self):
         """String represintaion for Rec"""
