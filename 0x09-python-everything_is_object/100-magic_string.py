458269f672a56ef780c2d69cac56fb81f13ed252
SeifEleslam <seifgouda98@gmail.com>
2023-11-28 23:38:15 +0200

    29-answer

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""Magic String Module"""
-CALL_COUNT = 0
-
-
 def magic_string():
-    """Returns a Magic string."""
-    global CALL_COUNT
-    output = "BestSchool, " * CALL_COUNT + "BestSchool"
-    CALL_COUNT += 1
-    return output
+    global i
+    return "BestSchool, " * i + "BestSchool"
