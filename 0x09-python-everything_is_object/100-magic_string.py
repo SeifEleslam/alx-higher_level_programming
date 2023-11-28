e89354095999d56e2d68967ed3862c3a0a6a1d03
SeifEleslam <seifgouda98@gmail.com>
2023-11-28 23:49:31 +0200

    29-answer

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def magic_string(ls=[]):
     ls += [1]
-    return "BestSchool, " * len(ls)-1 + "BestSchool"
+    return "BestSchool, " * (len(ls)-1) + "BestSchool"
+
+
+for i in range(10):
+    print(magic_string())
