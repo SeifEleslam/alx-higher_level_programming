2d927e62b21b46b02bf0b1c98f06463890fa3997
SeifEleslam <seifgouda98@gmail.com>
2023-12-04 20:40:54 +0200

    task2

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to contain is_kind_of_class function"""
+
+
+def inherits_from(obj, a_class):
+    """Return if the obj is instance of class"""
+    return isinstance(obj, a_class)
