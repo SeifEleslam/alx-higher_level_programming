b44ee2301609df7c297db060319f095497e73720
SeifEleslam <seifgouda98@gmail.com>
2023-12-06 00:53:01 +0200

    task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,5 +5,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == "__main__":
-    my_list = sys.argv[1:]
-    save_to_json_file(my_list, "add_item.json")
+    fileName = "add_item.json"
+    try:
+        my_list = load_from_json_file("fileName")
+    except Exception:
+        my_list = []
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, fileName)
