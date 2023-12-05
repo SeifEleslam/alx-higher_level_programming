6a47479f718c585ba0dd62a64eb04ccbc0ef7850
SeifEleslam <seifgouda98@gmail.com>
2023-12-06 00:44:00 +0200

    task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task8 Module"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+my_list = sys.argv[1:]
+save_to_json_file(my_list, "add_item.json")
