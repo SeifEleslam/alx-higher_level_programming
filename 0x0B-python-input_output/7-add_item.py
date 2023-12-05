3e2e07ba7eda402966de3295234ab75e61a39242
SeifEleslam <seifgouda98@gmail.com>
2023-12-06 00:53:34 +0200

    task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 if __name__ == "__main__":
     fileName = "add_item.json"
     try:
-        my_list = load_from_json_file("fileName")
+        my_list = load_from_json_file(fileName)
     except Exception:
         my_list = []
     my_list.extend(sys.argv[1:])
