6494e799f2ee6310f6f65e026ae0c90ac5dd0a31
SeifEleslam <seifgouda98@gmail.com>
2023-12-06 03:17:11 +0200

    test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -22,16 +22,20 @@ def signal_handler(signal_num, frame):
 
 if __name__ == "__main__":
     count = 0
-    signal.signal(signal.SIGINT, signal_handler)
-# try:
-    for line in sys.stdin:
-        count += 1
-        line = line.strip()
-        line_data = line.split(" ")
-        data["File size"] += int(line_data[-1])
-        data["status"][line_data[-2]] = data["status"][line_data[-2]] + \
-            1 if line_data[-2] in data["status"] else 1
-        if count % 10 == 0:
-            print_stats(data)
-# except KeyboardInterrupt:
+    data = {"File size": 0, "status": {}}
+
+    # signal.signal(signal.SIGINT, signal_handler)
+    try:
+        for line in sys.stdin:
+            count += 1
+            line = line.strip()
+            line_data = line.split(" ")
+            data["File size"] += int(line_data[-1])
+            data["status"][line_data[-2]] = data["status"][line_data[-2]] + \
+                1 if line_data[-2] in data["status"] else 1
+            if count % 10 == 0:
+                print_stats(data)
+    except KeyboardInterrupt:
+        print_stats(data)
+        raise
     print_stats(data)
