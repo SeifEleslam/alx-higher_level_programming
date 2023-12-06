c14c90a4f8f601601c57a769ebf1504ca075ac2b
SeifEleslam <seifgouda98@gmail.com>
2023-12-06 02:02:11 +0200

    task9

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Pascal Module"""
+
+
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1]
+        for c in range(1, i):
+            row.append(triangle[-1][c] + triangle[-1][c-1])
+        if i > 0:
+            row.append(1)
+        triangle.append(row)
+    return triangle
