28955f4ea3c10ec3d0ffb6b6064f43eec2ec62b4
SeifEleslam <seifgouda98@gmail.com>
2023-12-11 01:49:01 +0200

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,11 +16,13 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Convert a list of dictionaries to a JSON string."""
         list_dictionaries = [] if list_dictionaries is None else list_dictionaries
         return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
+        """to json"""
         json_list = json.loads(json_string)
         return [] if json_list is None else json_list
 
@@ -35,6 +37,16 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        """Create an object from a dictionary."""
         shape = cls(1) if cls.__name__ == "Square"else cls(1, 1)
         cls.update(shape, **dictionary)
         return shape
+
+    @classmethod
+    def load_from_file(cls):
+        """Load the list of objects from a file."""
+        try:
+            with open(f"{cls.__name__}.json", "r") as file:
+                return list(map(lambda x: cls.create(**x), cls.from_json_string(file.read())))
+        except Exception as e:
+            return []
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -47,12 +47,3 @@ class Square(Rectangle):
                 newDic[key.replace("_", "").replace(
                     "Rectangle", "").replace("Square", "")] = val
         return newDic
-
-
-r1 = Rectangle(3, 5, 1)
-r1_dictionary = r1.to_dictionary()
-r2 = Rectangle.create(**r1_dictionary)
-print(r1)
-print(r2)
-print(r1 is r2)
-print(r1 == r2)
