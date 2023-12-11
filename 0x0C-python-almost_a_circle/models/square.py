26425697ac1c8c27c4c7d203a7ba6938a7ee3a92
SeifEleslam <seifgouda98@gmail.com>
2023-12-11 20:41:07 +0200

    draw function

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize Square"""
-        super().__init__(size, size, x, y, id)
+        super().__init__(id)
         self.size = size
         self.x = x
         self.y = y
