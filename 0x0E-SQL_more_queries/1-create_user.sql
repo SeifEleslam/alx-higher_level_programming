c1c250cfe82e50a08a42a12c90f3e860b2e7a56a
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 00:52:04 +0200

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- USER PRIVILEGES
-CREATE USER IF NOT EXISTS user_0d_1@localhost;
-SET PASSWORD FOR user_0d_1@localhost = PASSWORD('user_0d_1_pwd');
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY user_0d_1_pwd;
