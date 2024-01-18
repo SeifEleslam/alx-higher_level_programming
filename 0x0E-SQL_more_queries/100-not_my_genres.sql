01af561a467c31188e536da854ace9274838a67d
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 04:35:01 +0200

    force_name

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,15 +1,13 @@
 -- Counting the number of shows with each genre
-SELECT DISTINCT tv_genres.name,
-    tv_shows.title
+SELECT DISTINCT tv_genres.name
 FROM tv_genres
     LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
+WHERE tv_genres.name NOT IN (
+        SELECT name
+        FROM tv_genres
+            INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_genres.id
+            INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.genre_id
+        WHERE tv_shows.title = 'Dexter'
+    )
 ORDER BY tv_genres.name;
---  (
---         SELECT title
---         FROM tv_shows
---             INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
---             INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
---         WHERE tv_genres.name = "Comedy"
---     )
