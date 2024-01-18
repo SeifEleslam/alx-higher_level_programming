a83eab58cae31d414fe656f962395fbc3f21c03c
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 03:29:50 +0200

    force_name

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++
@@ -1,7 +0,0 @@
--- Listing 
-SELECT tv_genres.name as genre,
-    COUNT(*) as number_of_shows
-FROM tv_genres
-    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC;
