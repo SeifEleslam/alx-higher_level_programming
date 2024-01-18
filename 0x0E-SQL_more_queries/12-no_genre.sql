ef570309287437968fa6132298e2674390d835c1
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 03:14:57 +0200

    force_name

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,8 @@
 -- force_name TABLE
-SELECT tv_genres.name as genre,
-    COUNT(tv_show_genres.id) as number_of_shows
-FROM tv_genres
-    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title,
+    tv_show_genres.genre_id
+FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title,
+    tv_show_genres.genre_id;
