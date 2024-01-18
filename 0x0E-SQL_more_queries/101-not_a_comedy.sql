8cdc063e7a25ee12e816522202060665cbf7ce55
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 04:18:50 +0200

    force_name

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,5 @@
 -- Counting the number of shows with each genre
-SELECT DISTINCT tv_shows.title,
-    tv_genres.name
+SELECT DISTINCT tv_shows.title
 FROM tv_shows
     LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
