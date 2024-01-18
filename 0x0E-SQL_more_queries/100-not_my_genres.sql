b2cd626c2c26ef7bfb25537a7f39612dca632b88
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 04:31:55 +0200

    force_name

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,4 +4,11 @@ FROM tv_genres
     LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_shows.title;
+ORDER BY tv_genres.name;
+--  (
+--         SELECT title
+--         FROM tv_shows
+--             INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+--             INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+--         WHERE tv_genres.name = "Comedy"
+--     )
