3581ace0c094b3dd882edacd55b2c8a5e885528a
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 04:46:20 +0200

    force_name

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,7 @@
 -- Counting the number of shows with each genre
-SELECT tv_genres.name,
+SELECT tv_shows.title,
     SUM(tv_show_ratings.rate) AS rating
-FROM tv_genres
-    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+FROM tv_shows
     INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_genres.name
+GROUP BY tv_shows.title
 ORDER BY rating DESC;
