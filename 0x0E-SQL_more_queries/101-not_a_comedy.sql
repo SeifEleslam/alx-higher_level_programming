f6c5bb0eaad201fd2101cc497b373535ec5f6f6e
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 04:15:11 +0200

    force_name

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,7 +2,5 @@
 SELECT DISTINCT tv_shows.title
 FROM tv_shows
     LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name <> 'Comedy'
-    OR NULL
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id -- WHERE tv_genres.name NOT IN
 ORDER BY tv_shows.title;
