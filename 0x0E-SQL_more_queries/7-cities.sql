981b508452d8d9f6c645db4b85b9aa9dc043d0bc
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 02:32:34 +0200

    force_name

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- force_name TABLE
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+);
