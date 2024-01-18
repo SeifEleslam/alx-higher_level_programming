46fc485c2f1c65cc6efb7f41c261f7d67eec8df5
SeifEleslam <seifgouda98@gmail.com>
2024-01-18 03:46:33 +0200

    force_name

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,4 @@ SELECT tv_genres.name,
     INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY genre;
+ORDER BY tv_genres.name;
