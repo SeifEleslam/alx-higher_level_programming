109cd2be71243af7d6642bc5c64a637ee3c3cef6
SeifEleslam <seifgouda98@gmail.com>
2024-02-13 00:52:46 +0200

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 if (process.argv.length > 3) console.log('Arguments found');
 else if (process.argv.length > 2) console.log('Argument found');
 else console.log('No argument');
