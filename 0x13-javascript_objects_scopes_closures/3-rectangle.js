f2e64f0fd621f4fdd9b120c1e36aa82d8de162cf
SeifEleslam <seifgouda98@gmail.com>
2024-02-13 21:31:17 +0200

    task3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// prettier-ignore
+class Rectangle {
+  constructor (w, h) {
+    if (!w || !h || w <= 0 || h <= 0) return;
+    this.width = w;
+    this.height = h;
+  }
+  print() {
+    if (!this.width || !this.height) return;
+    for (let i = 1; i <= this.height; i++) console.log('X'.repeat(this.width));
+  }
+}
+
+module.exports = Rectangle;
