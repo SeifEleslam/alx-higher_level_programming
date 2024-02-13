aec17dacbc0b5e613e486244a18ced5ebc81401b
SeifEleslam <seifgouda98@gmail.com>
2024-02-13 22:08:04 +0200

    task4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,24 +3,24 @@
 // prettier-ignore
 class Rectangle {
   constructor (w, h) {
-    if (!w || !h || w <= 0 || h <= 0) return;
-    this.width = w;
-    this.height = h;
+    if (!w || !h || w <= 0 || h <= 0) return
+    this.width = w
+    this.height = h
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      console.log('X'.repeat(this.width))
     }
   }
 
   rotate () {
-    [this.height, this.width] = [this.width, this.height];
+    [this.height, this.width] = [this.width, this.height]
   }
-  
+
   double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
+    this.width *= 2
+    this.height *= 2
   }
 }
 
