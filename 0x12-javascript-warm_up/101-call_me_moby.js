cdd08883d778276362414aceaec15966db7bc5ac
SeifEleslam <seifgouda98@gmail.com>
2024-02-13 03:08:40 +0200

    task 101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // prettier-ignore
 function callMeMoby (x, theFunction) {
-    for (let i = 0; i < x; i++)
-        theFunction();
+  for (let i = 0; i < x; i++) theFunction();
 }
 exports.callMeMoby = callMeMoby;
