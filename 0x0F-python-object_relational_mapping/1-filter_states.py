641305b3135ea756212e422f16f9d47d60a55933
SeifEleslam <seifgouda98@gmail.com>
2024-02-16 14:17:26 +0200

    add main

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,14 +4,15 @@
 import MySQLdb as db
 from sys import argv as args
 
-conn = db.connect(
-    host="localhost", port=3306,
-    user=args[1], passwd=args[2],
-    db=args[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    conn = db.connect(
+        host="localhost", port=3306,
+        user=args[1], passwd=args[2],
+        db=args[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
