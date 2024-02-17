eb7aac48e32d83cc7455b90586a7a3276f4816dd
SeifEleslam <seifgouda98@gmail.com>
2024-02-18 00:35:23 +0200

    task14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
 import sys
-from model_state import Base
+from model_state import Base, State
 from sqlalchemy.orm import Session
 from model_city import City
 from sqlalchemy import (create_engine)
