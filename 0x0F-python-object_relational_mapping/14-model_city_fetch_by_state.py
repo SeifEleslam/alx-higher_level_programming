ac874425855252d7f162fa9fa312955ef9f6d9dd
SeifEleslam <seifgouda98@gmail.com>
2024-02-18 00:33:07 +0200

    task14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
 import sys
-from model_state import Base, State
+from model_state import Base
 from sqlalchemy.orm import Session
-
+from model_city import City
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
@@ -11,9 +11,9 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    selected_states = session.query(State)\
-        .filter(State.name.contains('a'))
-    for state in selected_states:
-        session.delete(state)
-    session.commit()
+    selected_cities = session.query(City)\
+        .all()
+    for city in selected_cities:
+        print("{}: ({}) {}".format(
+            city.state.name, city.id, city.name))
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from model_state import Base
+from model_state import Base, State
 
 
 class City(Base):
