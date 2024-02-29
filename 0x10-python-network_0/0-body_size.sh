3d54bc4f5c6cf2966aab0cdf8b9985958a04a49d
SeifEleslam <seifgouda98@gmail.com>
2024-02-29 21:20:20 +0200

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # Using curl to get response size of provided url
+curl -s -o /dev/null  -w "%{size_download}\n" "$1"
 
-url=$1
-if [ -z "$url" ] ; then
-  echo "Usage: $0 <URL>"
-  exit 1
-else
-  curl -s -o /dev/null  -w "%{size_download}\n" "$url"
-fi
