728d58f4f85829fb436d9b1cc181dd25e38f4247
SeifEleslam <seifgouda98@gmail.com>
2024-03-01 02:25:33 +0200

    task6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Find peak module"""
+
 
 def find_peak(list_of_integers):
     """Finds the peak element in a list"""
@@ -10,12 +12,3 @@ def find_peak(list_of_integers):
                 list_of_integers[i] > list_of_integers[i+1]:
             break
     return peak
-
-
-# Testing the function with various inputs
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
