ecff95472a0a44a2a2437569940c4a6ba581ebf9
SeifEleslam <seifgouda98@gmail.com>
2024-03-01 03:51:47 +0200

    task102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Using curl to get response size of provided url
-curl -so /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
+curl -so /dev/null -w "You got me!\n" 0.0.0.0:5000/catch_me
