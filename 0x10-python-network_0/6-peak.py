d52409fbacc3eac1f9aa13fa5a326a706f7b8945
SeifEleslam <seifgouda98@gmail.com>
2024-03-01 02:56:05 +0200

    task5

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -26,11 +26,3 @@ def find_peak(list_of_integers):
     #             list_of_integers[i] > list_of_integers[i+1]:
     #         break
     return find_peak(r_list)
-
-
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
