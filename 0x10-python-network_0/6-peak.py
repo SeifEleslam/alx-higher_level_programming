86eaa8edd51bf7645140f1bd43381281e9562269
SeifEleslam <seifgouda98@gmail.com>
2024-03-01 02:52:31 +0200

    task5

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,9 +13,12 @@ def find_peak(list_of_integers):
         return max(list_of_integers[0],
                    list_of_integers[1])
     target = int(list_len/2)
+    if list_of_integers[target] > list_of_integers[target+1] and\
+            list_of_integers[target] > list_of_integers[target-1]:
+        return list_of_integers[target]
     if list_of_integers[target] < list_of_integers[target-1]:
         r_list = list_of_integers[:target]
-    r_list = list_of_integers[target:]
+    r_list = list_of_integers[target+1:]
     # Check if there is only one
     # for i in range(list_len):
     #     peak = list_of_integers[i]
@@ -23,3 +26,11 @@ def find_peak(list_of_integers):
     #             list_of_integers[i] > list_of_integers[i+1]:
     #         break
     return find_peak(r_list)
+
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
