1785226e3ce7923f0a5e95f6cb749b7876bc2410
SeifEleslam <seifgouda98@gmail.com>
2024-03-01 02:46:52 +0200

    task6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,11 +4,22 @@
 
 def find_peak(list_of_integers):
     """Finds the peak element in a list"""
-    peak = None
+    list_len = len(list_of_integers)
+    if list_len == 0:
+        return None
+    if list_len == 1:
+        return list_of_integers[0]
+    if list_len == 2:
+        return max(list_of_integers[0],
+                   list_of_integers[1])
+    target = int(list_len/2)
+    if list_of_integers[target] < list_of_integers[target-1]:
+        r_list = list_of_integers[:target]
+    r_list = list_of_integers[target:]
     # Check if there is only one
-    for i in range(len(list_of_integers)):
-        peak = list_of_integers[i]
-        if i+1 < len(list_of_integers) and\
-                list_of_integers[i] > list_of_integers[i+1]:
-            break
-    return peak
+    # for i in range(list_len):
+    #     peak = list_of_integers[i]
+    #     if i+1 < list_len and\
+    #             list_of_integers[i] > list_of_integers[i+1]:
+    #         break
+    return find_peak(r_list)
