ef3487ba68ed18eeedc0bdb3929fd54e66d45fd3
SeifEleslam <seifgouda98@gmail.com>
2024-03-01 04:30:54 +0200

    task102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Using curl to get response size of provided url
-curl -H "Origin: School" -d "user_id=98" -LX PUT  $1
+curl -H "Origin: School" -d "user_id=98" -LX PUT 0.0.0.0:5000/catch_me 
