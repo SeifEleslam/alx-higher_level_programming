7642c5e63f41ac9ba2eb8474d8a631ce36b667fd
SeifEleslam <seifgouda98@gmail.com>
2024-03-27 01:45:47 +0200

    test

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const fs = require('fs');
+const { readFile } = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) console.log(err);
   else console.log(data);
 });
