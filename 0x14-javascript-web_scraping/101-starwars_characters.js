2c7b5d6bbf9043b9f8b649147eb6ec8e0f2906dc
SeifEleslam <seifgouda98@gmail.com>
2024-03-27 03:08:55 +0200

    task101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,14 +3,15 @@ const { get } = require('request');
 
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-function callbackPerson(people, idx) {
-  if (idx < people.length)
+const callbackPerson = (people, idx) => {
+  if (idx < people.length) {
     get(people[idx], null, (_, personRes) => {
       const person = JSON.parse(personRes.body);
       console.log(person.name);
       callbackPerson(people, idx + 1);
     });
-}
+  }
+};
 
 get(url, null, (_, res) => {
   const people = JSON.parse(res.body).characters;
